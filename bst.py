e761544594b6c4f9105ee02071c2d18b8539bd4d
KENSEI <107671402+KENSEI-dev@users.noreply.github.com>
2025-10-06 21:51:29 +0530

    bst2

--- bst.py
+++ bst.py
@@ -20,6 +20,21 @@ class BinaryTree:
                 print(" " * (space + 4) + "here it is.")
             self.display_tree(root.left, space + 4, "L-->", val)
             self.display_tree(root.right, space + 4, "R-->", val)
+    def preorder_traversal(self,root):
+        if root is not None:
+            print(root.data, end=" ")
+            self.preorder_traversal(root.left)
+            self.preorder_traversal(root.right)
+    def postorder_traversal(self,root):
+        if root is not None:
+            self.postorder_traversal(root.left)
+            self.postorder_traversal(root.right)
+            print(root.data, end=" ")
+    def inorder_traversal(self,root):
+        if root is not None:
+            self.inorder_traversal(root.left)
+            print(root.data,end=" ")
+            self.inorder_traversal(root.right)
 if __name__ == "__main__":
     
     tree = BinaryTree(10)
@@ -27,8 +42,14 @@ if __name__ == "__main__":
     tree.root.right = Node(20)
     tree.root.left.left = Node(3)
     tree.root.left.right = Node(7)
-    tree.display_tree(tree.root, 0, "Root:", )
+    tree.display_tree(tree.root, 0, "Root:")
     result = tree.searchBST(tree.root, 5)
+    print("\nPreorder traversal: ")
+    tree.preorder_traversal(tree.root)
+    print("\nPostorder traversal: ")
+    tree.postorder_traversal(tree.root)
+    print("\nInorder traversal: ")
+    tree.inorder_traversal(tree.root)
     '''if result is not None and result.data == 5:
         print("Finding value 5 in BST : True")
     else:
